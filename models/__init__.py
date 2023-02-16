cfb44d5ab959da02cd5f0195ea27f8596b088aba
Jo <69627271+jo-esign@users.noreply.github.com>
2023-02-16 09:07:36 +0100

    AirBnB

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""create a unique FileStorage instance for the application"""
-
+"""Initilizing FileStorge engine
+"""
 from models.engine.file_storage import FileStorage
+
 
 storage = FileStorage()
 storage.reload()
